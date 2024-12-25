dbbcb103a5aeb10804d43816bb0ca88d27fe99b7
Ussama Azam <nuclearoreo@gmail.com>
2024-12-24 20:19:32 -0500

    Shared ownership and references

---
+++ src/chapter_1/shared_ownership_and_reference_counting.rs
@@ -0,0 +1,42 @@
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+    use std::thread;
+
+    // Shared ownership with statics
+    // Note: Statics are shared across threads and the entire program
+    // Note: Static exist for the entire lifetime of the program
+    // Note: Statics exist before the main function is called
+    static X: [i32; 3] = [1, 2, 3];
+    #[test]
+    fn shared_ownership_with_statics() {
+        thread::spawn(|| dbg!(&X)).join().unwrap();
+        thread::spawn(|| dbg!(&X)).join().unwrap();
+    }
+
+    // Shared ownership with leaky references
+    // Note: Leaky references are shared across threads and the entire program
+    // Note: The "move" keyword is used to transfer ownership of the "reference" to the thread
+    // Note: The "Box::leak" function is used to create a leaky reference
+    #[test]
+    fn shared_ownership_with_leaky_references() {
+        let x: &'static [i32; 3] = Box::leak(Box::new([1, 2, 3]));
+        thread::spawn(move || dbg!(x)).join().unwrap();
+        thread::spawn(move || dbg!(x)).join().unwrap();
+    }
+
+    // Shared ownership with Arc
+    // Note: Arc is a reference counting pointer
+    // Note: Arc is used in multi-threaded context
+    #[test]
+    fn shared_ownership_with_arc() {
+        let x = Arc::new([1, 2, 3]);
+        let x1 = x.clone();
+        let x2 = x.clone();
+
+        assert_eq!(x1.as_ptr(), x2.as_ptr());
+
+        thread::spawn(move || dbg!(x1)).join().unwrap();
+        thread::spawn(move || dbg!(x2)).join().unwrap();
+    }
+}
